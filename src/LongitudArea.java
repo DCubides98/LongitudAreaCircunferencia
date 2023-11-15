fb2642803167bd693c1b22aefab098bb5972727c
ECM9749E <sebas@MacBook-Air-de-Sebastian.local>
2023-11-14 23:45:02 -0500

    Longitud y area de circunferencia

--- src/LongitudArea.java
+++ src/LongitudArea.java
@@ -18,6 +18,7 @@ double longitud = 2 * 3.14159 * radio;
     System.out.println("La longitud de la circunferencia es: " + longitud);
     System.out.println("El area de la circunferencia es: " + area);
 
+scanner.close();
 
 }
 
